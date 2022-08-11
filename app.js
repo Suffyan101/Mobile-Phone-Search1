c7e1f1d729a7851649a5369a06b0bc0df79ecbc5
Suffyan101 <suffyan.r01@gmail.com>
2022-08-12 03:04:21 +0500

    update

---
+++ app.js
@@ -0,0 +1,108 @@
+var sumsung = {
+    S6:{
+        Name: "Sumsung S6",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    S7:{
+        Name: "Sumsung S7",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    S8:{
+        Name: "Sumsung S8",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    S9:{
+        Name: "Sumsung S9",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    S10:{
+        Name: "Sumsung S10",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    S20:{
+        Name: "Sumsung S20",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+}
+
+var iphone = {
+    iphone7:{
+        Name: "Iphone 7",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    iphone8:{
+        Name: "Iphone 8",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    iphone8plus:{
+        Name: "Iphone 8 Plus",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    iphonex:{
+        Name: "Iphone X",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    iphonexmax:{
+        Name: "Iphone X-Max",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    iphonexsmax:{
+        Name: "Iphone Xs-Max",
+        Price: "$1200",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+}
+
+var realme ={
+    realme3:{
+        Name: "Realme 3",
+        Price: "$120",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    realme5:{
+        Name: "Realme 5",
+        Price: "$120",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+    realme6:{
+        Name: "Realme 6",
+        Price: "$120",
+        Colour:["Black","Red","White"],
+        Camera:"18mp",
+    },
+}
+var c = ['s6','s7','s8']
+
+
+var a = document.getElementById("select")
+var b = document.getElementById("search").value
+function on_click(){
+    if(a.value === "iphone" || a.value === "sumsung" || a.value === "realme"){
+        if(b ===)
+    }
+}
