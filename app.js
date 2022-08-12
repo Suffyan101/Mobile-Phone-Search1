e44ca2544ef140022620594a9e9fb9d71c39acc6
Suffyan101 <suffyan.r01@gmail.com>
2022-08-13 03:15:49 +0500

    update

--- app.js
+++ app.js
@@ -1,119 +1,146 @@
-var sumsung = {
-    S6:{
-        Name: "Sumsung S6",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
+var mobile = {
+    sumsung: {
+        S6: {
+            Name: "Sumsung S6",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        S7: {
+            Name: "Sumsung S7",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        S8: {
+            Name: "Sumsung S8",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        S9: {
+            Name: "Sumsung S9",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        S10: {
+            Name: "Sumsung S10",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        S20: {
+            Name: "Sumsung S20",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
     },
-    S7:{
-        Name: "Sumsung S7",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-    S8:{
-        Name: "Sumsung S8",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-    S9:{
-        Name: "Sumsung S9",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-    S10:{
-        Name: "Sumsung S10",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-    S20:{
-        Name: "Sumsung S20",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-}
 
-var iphone = {
-    iphone7:{
-        Name: "Iphone 7",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
+    iphone: {
+        iphone7: {
+            Name: "Iphone 7",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        iphone8: {
+            Name: "Iphone 8",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        iphone8plus: {
+            Name: "Iphone 8 Plus",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        iphonex: {
+            Name: "Iphone X",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        iphonexmax: {
+            Name: "Iphone X-Max",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        iphonexsmax: {
+            Name: "Iphone Xs-Max",
+            Price: "$1200",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
     },
-    iphone8:{
-        Name: "Iphone 8",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-    iphone8plus:{
-        Name: "Iphone 8 Plus",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-    iphonex:{
-        Name: "Iphone X",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-    iphonexmax:{
-        Name: "Iphone X-Max",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-    iphonexsmax:{
-        Name: "Iphone Xs-Max",
-        Price: "$1200",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-}
 
-var realme ={
-    realme3:{
-        Name: "Realme 3",
-        Price: "$120",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-    realme5:{
-        Name: "Realme 5",
-        Price: "$120",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-    realme6:{
-        Name: "Realme 6",
-        Price: "$120",
-        Colour:["Black","Red","White"],
-        Camera:"18mp",
-    },
-}
-var c = ['s6','s7','s8','s9','s10','s20',
-'realme3','realme5','realme6','iphone7','iphone8',
-'iphone8plus','iphonex','iphonexmax','iphonexsmax']
-
-
-var a = document.getElementById("select")
-var b = document.getElementById("search")
-var d = b.value
-var e = c.indexOf('d')
-function on_click(){
-    if(a.value === "iphone" || a.value === "sumsung" || a.value === "realme"){
-        if(e !== -1){
-            if(a.value === "iphone"){
-                document.getElementById('demo').innerHTML=a.value['d'].Name
-            }
-        }
-        else{
-            document.write("Result Not Found")
-        }
+    realme: {
+        realme3: {
+            Name: "Realme 3",
+            Price: "$120",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        realme5: {
+            Name: "Realme 5",
+            Price: "$120",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
+        realme6: {
+            Name: "Realme 6",
+            Price: "$120",
+            Colour: ["Black", "Red", "White"],
+            Camera: "18mp",
+        },
     }
 }
+function generateHtml(brand, price, color, features) {
+    var div = document.createElement("div");
+    var h4 = document.createElement("h4");
+    var h4text = document.createTextNode(brand)
+    h4.appendChild(h4text)
+    var h3 = document.createElement("h3");
+    var h3text = document.createTextNode(features)
+    h3.appendChild(h3text)
+    var h2 = document.createElement("h2");
+    var h2text = document.createTextNode(price)
+    h2.appendChild(h2text)
+    var h1 = document.createElement("h1");
+    var h1text = document.createTextNode(color)
+    h1.appendChild(h1text)
+    div.appendChild(h4)
+    div.appendChild(h3)
+    div.appendChild(h2)
+    div.appendChild(h1)
+
+    return div;
+}
+
+// console.log(res)
+var main = document.getElementById("demo");
+
+
+function on_click() {
+    var a = document.getElementById("select").value;
+    var b = document.getElementById("search").value;
+    // var result = mobile[a][b]
+    // var c = mobile
+    
+    for (keys in mobile[a]) {
+        var result =mobile[a]
+        console.log(result.)
+        // if(b === result){
+        //     result = mobile[a][keys].Name
+        //     break
+        // }
+        // else{
+        //     console.log("not found")
+        // }
+        
+    }
+}
+// main.appendChild(generateHtml(result.Name, result.Price, result.Colour, result.Camera));
